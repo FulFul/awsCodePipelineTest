82d8643948cc2bec333e2b155866338e88650f43
Grzegorz Fula <grzegorz.fula@gft.com>
2023-11-30 09:40:35 +0100

    my app

---
+++ src/main/java/com/fula/springexamplerest/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.fula.springexamplerest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class HelloController {
+
+
+    @GetMapping("/test/{name}")
+    public ResponseEntity<String> encryptToken(@PathVariable String name) {
+        return ResponseEntity.ok("Siema " + name + " mordo!");
+    }
+
+}
